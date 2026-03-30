3cd6194dd460f77411b0d472c9fcb1d718280390
Vitaly <pvitaly91@gmail.com>
2026-03-30 22:40:29 +0300

    host: make codex run instance ids unique per launch

--- include/common/logger.h
+++ include/common/logger.h
@@ -61,6 +61,7 @@ struct LoggerSessionInfo
     bool configured = false;
     std::filesystem::path log_directory;
     std::string session_id;
+    std::string run_instance_id;
     std::string run_label;
     std::uintmax_t max_file_size_bytes = 0;
     std::filesystem::path current_file_path;
