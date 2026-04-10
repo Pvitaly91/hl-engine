e8e4873cd8e785928069663ebeb7f32bba03baa0
Vitaly <pvitaly91@gmail.com>
2026-04-11 01:06:56 +0300

    Add dedicated selective signon multi-message fetch surface

--- include/app/launch_options.h
+++ include/app/launch_options.h
@@ -84,6 +84,9 @@ struct LaunchOptions
     bool signon_message_fetch_surface_enabled = false;
     bool signon_message_fetch_probe_enabled = false;
     std::string signon_message_fetch_probe_scenario = "happy";
+    bool signon_multi_message_fetch_surface_enabled = false;
+    bool signon_multi_message_fetch_probe_enabled = false;
+    std::string signon_multi_message_fetch_probe_scenario = "happy";
     int frame_count = 1000;
     float frame_time = 0.05f;
     int think_limit = 32;
--- src/app/host_application.cpp
+++ src/app/host_application.cpp
@@ -22516,6 +22516,12 @@ bool HostApplication::RunServerEngineShim(
         options.signon_message_fetch_probe_enabled;
     init_options.signon_message_fetch_probe_scenario =
         options.signon_message_fetch_probe_scenario;
+    init_options.signon_multi_message_fetch_surface_enabled =
+        options.signon_multi_message_fetch_surface_enabled;
+    init_options.signon_multi_message_fetch_probe_enabled =
+        options.signon_multi_message_fetch_probe_enabled;
+    init_options.signon_multi_message_fetch_probe_scenario =
+        options.signon_multi_message_fetch_probe_scenario;
     init_options.frame_bootstrap.frames = options.frame_count;
     init_options.frame_bootstrap.frametime = options.frame_time;
     init_options.frame_bootstrap.think_limit = options.think_limit;
@@ -22628,7 +22634,13 @@ bool HostApplication::RunServerEngineShim(
         + ", signon_message_fetch_probe="
         + std::string(init_options.signon_message_fetch_probe_enabled ? "1" : "0")
         + ", signon_message_fetch_probe_scenario="
-        + init_options.signon_message_fetch_probe_scenario);
+        + init_options.signon_message_fetch_probe_scenario
+        + ", signon_multi_message_fetch_surface="
+        + std::string(init_options.signon_multi_message_fetch_surface_enabled ? "1" : "0")
+        + ", signon_multi_message_fetch_probe="
+        + std::string(init_options.signon_multi_message_fetch_probe_enabled ? "1" : "0")
+        + ", signon_multi_message_fetch_probe_scenario="
+        + init_options.signon_multi_message_fetch_probe_scenario);
 
     common::Logger::Info(
         common::LogCategory::Startup,
