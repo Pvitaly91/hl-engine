1ce6a50c81024d0162b7f04812f89de04d7c30dc
Vitaly <pvitaly91@gmail.com>
2026-04-11 17:14:02 +0300

    HL-CL-20260411-132 Add dedicated signon cursor resume denial surface

--- src/app/host_application.cpp
+++ src/app/host_application.cpp
@@ -22546,6 +22546,12 @@ bool HostApplication::RunServerEngineShim(
         options.signon_message_cursor_eof_probe_enabled;
     init_options.signon_message_cursor_eof_probe_scenario =
         options.signon_message_cursor_eof_probe_scenario;
+    init_options.signon_message_cursor_resume_denial_surface_enabled =
+        options.signon_message_cursor_resume_denial_surface_enabled;
+    init_options.signon_message_cursor_resume_denial_probe_enabled =
+        options.signon_message_cursor_resume_denial_probe_enabled;
+    init_options.signon_message_cursor_resume_denial_probe_scenario =
+        options.signon_message_cursor_resume_denial_probe_scenario;
     init_options.frame_bootstrap.frames = options.frame_count;
     init_options.frame_bootstrap.frametime = options.frame_time;
     init_options.frame_bootstrap.think_limit = options.think_limit;
@@ -22688,7 +22694,15 @@ bool HostApplication::RunServerEngineShim(
         + ", signon_message_cursor_eof_probe="
         + std::string(init_options.signon_message_cursor_eof_probe_enabled ? "1" : "0")
         + ", signon_message_cursor_eof_probe_scenario="
-        + init_options.signon_message_cursor_eof_probe_scenario);
+        + init_options.signon_message_cursor_eof_probe_scenario
+        + ", signon_message_cursor_resume_denial_surface="
+        + std::string(
+            init_options.signon_message_cursor_resume_denial_surface_enabled ? "1" : "0")
+        + ", signon_message_cursor_resume_denial_probe="
+        + std::string(
+            init_options.signon_message_cursor_resume_denial_probe_enabled ? "1" : "0")
+        + ", signon_message_cursor_resume_denial_probe_scenario="
+        + init_options.signon_message_cursor_resume_denial_probe_scenario);
 
     common::Logger::Info(
         common::LogCategory::Startup,
