f08518c721415abc18eb3d7c2ad094d1546e24a7
Vitaly <pvitaly91@gmail.com>
2026-04-12 16:32:13 +0300

    Add carried-cursor target resume denial surface

--- src/app/host_application.cpp
+++ src/app/host_application.cpp
@@ -22576,6 +22576,12 @@ bool HostApplication::RunServerEngineShim(
         options.signon_message_cursor_carried_eof_probe_enabled;
     init_options.signon_message_cursor_carried_eof_probe_scenario =
         options.signon_message_cursor_carried_eof_probe_scenario;
+    init_options.signon_message_cursor_carried_resume_denial_surface_enabled =
+        options.signon_message_cursor_carried_resume_denial_surface_enabled;
+    init_options.signon_message_cursor_carried_resume_denial_probe_enabled =
+        options.signon_message_cursor_carried_resume_denial_probe_enabled;
+    init_options.signon_message_cursor_carried_resume_denial_probe_scenario =
+        options.signon_message_cursor_carried_resume_denial_probe_scenario;
     init_options.frame_bootstrap.frames = options.frame_count;
     init_options.frame_bootstrap.frametime = options.frame_time;
     init_options.frame_bootstrap.think_limit = options.think_limit;
@@ -22758,7 +22764,17 @@ bool HostApplication::RunServerEngineShim(
         + std::string(
             init_options.signon_message_cursor_carried_eof_probe_enabled ? "1" : "0")
         + ", signon_message_cursor_carried_eof_probe_scenario="
-        + init_options.signon_message_cursor_carried_eof_probe_scenario);
+        + init_options.signon_message_cursor_carried_eof_probe_scenario
+        + ", signon_message_cursor_carried_resume_denial_surface="
+        + std::string(
+            init_options.signon_message_cursor_carried_resume_denial_surface_enabled ? "1"
+                                                                                     : "0")
+        + ", signon_message_cursor_carried_resume_denial_probe="
+        + std::string(
+            init_options.signon_message_cursor_carried_resume_denial_probe_enabled ? "1"
+                                                                                   : "0")
+        + ", signon_message_cursor_carried_resume_denial_probe_scenario="
+        + init_options.signon_message_cursor_carried_resume_denial_probe_scenario);
 
     common::Logger::Info(
         common::LogCategory::Startup,
