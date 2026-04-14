e3caaacab3fa3fd8961d060a3d46293928567e44
Vitaly <pvitaly91@gmail.com>
2026-04-14 13:01:46 +0300

    HL-CL-20260411-149 claimed-range surface

--- src/app/host_application.cpp
+++ src/app/host_application.cpp
@@ -22607,6 +22607,12 @@ bool HostApplication::RunServerEngineShim(
     init_options
         .signon_message_cursor_carried_checkpoint_resume_token_claim_probe_scenario =
         options.signon_message_cursor_carried_checkpoint_resume_token_claim_probe_scenario;
+    init_options.signon_message_cursor_carried_checkpoint_claimed_range_surface_enabled =
+        options.signon_message_cursor_carried_checkpoint_claimed_range_surface_enabled;
+    init_options.signon_message_cursor_carried_checkpoint_claimed_range_probe_enabled =
+        options.signon_message_cursor_carried_checkpoint_claimed_range_probe_enabled;
+    init_options.signon_message_cursor_carried_checkpoint_claimed_range_probe_scenario =
+        options.signon_message_cursor_carried_checkpoint_claimed_range_probe_scenario;
     init_options.signon_message_cursor_carried_checkpoint_resume_range_surface_enabled =
         options.signon_message_cursor_carried_checkpoint_resume_range_surface_enabled;
     init_options.signon_message_cursor_carried_checkpoint_resume_range_probe_enabled =
@@ -22889,6 +22895,18 @@ bool HostApplication::RunServerEngineShim(
         + ", signon_message_cursor_carried_checkpoint_resume_token_claim_probe_scenario="
         + init_options
               .signon_message_cursor_carried_checkpoint_resume_token_claim_probe_scenario
+        + ", signon_message_cursor_carried_checkpoint_claimed_range_surface="
+        + std::string(
+            init_options.signon_message_cursor_carried_checkpoint_claimed_range_surface_enabled
+                ? "1"
+                : "0")
+        + ", signon_message_cursor_carried_checkpoint_claimed_range_probe="
+        + std::string(
+            init_options.signon_message_cursor_carried_checkpoint_claimed_range_probe_enabled
+                ? "1"
+                : "0")
+        + ", signon_message_cursor_carried_checkpoint_claimed_range_probe_scenario="
+        + init_options.signon_message_cursor_carried_checkpoint_claimed_range_probe_scenario
         + ", signon_message_cursor_carried_checkpoint_resume_range_surface="
         + std::string(
             init_options.signon_message_cursor_carried_checkpoint_resume_range_surface_enabled
