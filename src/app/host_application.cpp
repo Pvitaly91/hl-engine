67d73d5f2166ac21508b1fb242b9e9655c63fc46
Vitaly <pvitaly91@gmail.com>
2026-04-16 15:04:05 +0300

    HL-CL-20260411-165 claimant checkpoint resume-token claim surface

--- src/app/host_application.cpp
+++ src/app/host_application.cpp
@@ -22653,6 +22653,18 @@ bool HostApplication::RunServerEngineShim(
         options
             .signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_probe_scenario;
     init_options
+        .signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_claim_surface_enabled =
+        options
+            .signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_claim_surface_enabled;
+    init_options
+        .signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_claim_probe_enabled =
+        options
+            .signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_claim_probe_enabled;
+    init_options
+        .signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_claim_probe_scenario =
+        options
+            .signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_claim_probe_scenario;
+    init_options
         .signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_range_surface_enabled =
         options
             .signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_range_surface_enabled;
@@ -23112,6 +23124,21 @@ bool HostApplication::RunServerEngineShim(
         + ", signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_probe_scenario="
         + init_options
               .signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_probe_scenario
+        + ", signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_claim_surface="
+        + std::string(
+            init_options
+                    .signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_claim_surface_enabled
+                ? "1"
+                : "0")
+        + ", signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_claim_probe="
+        + std::string(
+            init_options
+                    .signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_claim_probe_enabled
+                ? "1"
+                : "0")
+        + ", signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_claim_probe_scenario="
+        + init_options
+              .signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_token_claim_probe_scenario
         + ", signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_range_surface="
         + std::string(
             init_options
