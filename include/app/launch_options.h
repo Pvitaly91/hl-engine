e89b974d6b23d729f068906292e5938bb1ce6de2
Vitaly <pvitaly91@gmail.com>
2026-04-17 12:06:49 +0300

    HL-CL-20260411-167 dedicated successor claimant checkpoint EOF surface

--- include/app/launch_options.h
+++ include/app/launch_options.h
@@ -174,6 +174,13 @@ struct LaunchOptions
     std::string
         signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_range_probe_scenario =
             "happy";
+    bool signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_eof_surface_enabled =
+        false;
+    bool signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_eof_probe_enabled =
+        false;
+    std::string
+        signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_eof_probe_scenario =
+            "happy";
     bool signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_range_surface_enabled =
         false;
     bool signon_message_cursor_carried_checkpoint_claimed_checkpoint_resume_range_probe_enabled =
