8c6ecc952d8ec4665f6cfb8a3cfb258bc644a22d
Vitaly <pvitaly91@gmail.com>
2026-04-19 00:54:35 +0300

    Add successor checkpoint resumed denial surface

--- src/app/host_application.cpp
+++ src/app/host_application.cpp
@@ -22761,6 +22761,18 @@ bool HostApplication::RunServerEngineShim(
         options
             .signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resume_eof_probe_scenario;
     init_options
+        .signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resumed_denial_surface_enabled =
+        options
+            .signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resumed_denial_surface_enabled;
+    init_options
+        .signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resumed_denial_probe_enabled =
+        options
+            .signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resumed_denial_probe_enabled;
+    init_options
+        .signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resumed_denial_probe_scenario =
+        options
+            .signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resumed_denial_probe_scenario;
+    init_options
         .signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_resume_range_surface_enabled =
         options
             .signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_resume_range_surface_enabled;
@@ -23427,6 +23439,21 @@ bool HostApplication::RunServerEngineShim(
         + ", signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resume_eof_probe_scenario="
         + init_options
               .signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resume_eof_probe_scenario
+        + ", signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resumed_denial_surface="
+        + std::string(
+            init_options
+                    .signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resumed_denial_surface_enabled
+                ? "1"
+                : "0")
+        + ", signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resumed_denial_probe="
+        + std::string(
+            init_options
+                    .signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resumed_denial_probe_enabled
+                ? "1"
+                : "0")
+        + ", signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resumed_denial_probe_scenario="
+        + init_options
+              .signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resumed_denial_probe_scenario
         + ", signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_resume_range_surface="
         + std::string(
             init_options
