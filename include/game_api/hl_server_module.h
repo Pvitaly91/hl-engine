a127fde3dcbce8826a248190087a288322da116e
Vitaly <pvitaly91@gmail.com>
2026-04-25 19:27:05 +0300

    Implement quinary checkpoint resume-denial surface

--- include/game_api/hl_server_module.h
+++ include/game_api/hl_server_module.h
@@ -13756,6 +13756,7 @@ struct DedicatedSignonMessageCursorCarriedCheckpointClaimedCheckpointSuccessorCh
     bool successor_checkpoint_resume_token_claim_resume_denial_enabled = false;
     bool requires_claimed_successor_checkpoint_resume_token_claim_eof_ready_session =
         true;
+    std::string claimant_depth = "quinary";
     int accepted = 0;
     int rejected = 0;
     std::string last_source_session;
@@ -13764,6 +13765,7 @@ struct DedicatedSignonMessageCursorCarriedCheckpointClaimedCheckpointSuccessorCh
     std::string last_successor_claimant_session;
     std::string last_tertiary_successor_claimant_session;
     std::string last_quaternary_claimant_session;
+    std::string last_quinary_claimant_session;
     std::string last_denied_cursor_id;
     bool resume_allowed = false;
     std::string denial_reason = "<unset>";
@@ -13804,6 +13806,7 @@ struct DedicatedSignonMessageCursorCarriedCheckpointClaimedCheckpointSuccessorCh
     int rejected = 0;
     std::string last_reject_reason = "<none>";
     std::string requested_quaternary_claimant_session;
+    std::string requested_quinary_claimant_session;
     std::string requested_cursor_id;
     std::string parsed_source_session;
     std::string parsed_issuer_target_session;
@@ -13811,6 +13814,8 @@ struct DedicatedSignonMessageCursorCarriedCheckpointClaimedCheckpointSuccessorCh
     std::string parsed_successor_claimant_session;
     std::string parsed_tertiary_successor_claimant_session;
     std::string parsed_quaternary_claimant_session;
+    std::string parsed_quinary_claimant_session;
+    std::string parsed_claimant_depth = "quinary";
     std::string parsed_cursor_id;
     bool parsed_resume_allowed = false;
     std::string parsed_denial_reason;
