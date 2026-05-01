6d253b56536217660892d3171af63836b808e401
Vitaly <pvitaly91@gmail.com>
2026-05-01 22:46:14 +0300

    HL-CL-20260411-252 fix resumed denial summary copy

--- include/app/launch_options.h
+++ include/app/launch_options.h
@@ -467,7 +467,8 @@ struct LaunchOptions
         false;
     std::string
         signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resume_token_claim_checkpoint_resume_token_claim_checkpoint_resume_token_claim_checkpoint_resume_eof_probe_scenario =
-            "happy";    bool signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resume_token_claim_checkpoint_resume_token_claim_checkpoint_resume_token_claim_checkpoint_resumed_denial_surface_enabled =
+            "happy";
+    bool signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resume_token_claim_checkpoint_resume_token_claim_checkpoint_resume_token_claim_checkpoint_resumed_denial_surface_enabled =
         false;
     bool signon_message_cursor_carried_checkpoint_claimed_checkpoint_successor_checkpoint_resume_token_claim_checkpoint_resume_token_claim_checkpoint_resume_token_claim_checkpoint_resumed_denial_probe_enabled =
         false;
