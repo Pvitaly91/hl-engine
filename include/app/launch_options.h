4530801e2f5b0ab55a1427af357f7e3cc8dd69ac
Vitaly <pvitaly91@gmail.com>
2026-05-06 23:53:58 +0300

    Add query info loopback regression acceptance gate

--- include/app/launch_options.h
+++ include/app/launch_options.h
@@ -124,6 +124,9 @@ struct LaunchOptions
     std::string
         hlds_query_info_byte_level_loopback_query_client_smoke_probe_scenario =
             "happy";
+    bool hlds_query_info_loopback_regression_acceptance_probe_enabled = false;
+    std::string hlds_query_info_loopback_regression_acceptance_probe_scenario =
+        "happy";
     bool hlds_serverinfo_contract_backed_diagnostic_path_integration_probe_enabled =
         false;
     std::string
